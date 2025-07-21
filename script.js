40e0a18ebfe452fdc5022645a5c800af1e7a89ad
BuiTheAnh203 <tamnguyen388832@gmail.com>
2025-07-22 01:02:20 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+let password = "";
+const correctPassword = "181206";
+const passwordInput = document.getElementById("password");
+
+function addNumber(number) {
+  if (password.length < 6) {
+    password += number;
+    passwordInput.value = password;
+    passwordInput.classList.add("active");
+  }
+}
+
+function clearPassword() {
+  password = "";
+  passwordInput.value = password;
+  passwordInput.classList.remove("active");
+}
+
+function checkPassword() {
+  if (password === correctPassword) {
+    alert("Mật khẩu đúng! Chẩn bị đón bất ngờ nè!!");
+    window.location.href = "sinhnhat.html";
+  } else {
+    passwordInput.classList.add("shake");
+    setTimeout(() => {
+      passwordInput.classList.remove("shake");
+      alert("Mật khẩu sai! Vui lòng thử lại.");
+      clearPassword();
+    }, 500);
+  }
+} 
